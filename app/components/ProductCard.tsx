b92838b916ce30c8f91143e65ecdfdcd78771ec3
nzemia <mualukofrank@gmail.com>
2024-06-22 12:53:13 +0300

    newest products

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,58 @@
+import { Button } from "@/components/ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+interface iAppProps {
+    images: string[];
+    name: string;
+    price: number;
+    smallDescription: string;
+    id: string;
+}
+export function ProductCard({ images, id, price, smallDescription, name,  } : iAppProps ) {
+    return(
+        <div className="rounded-lg">
+            <Carousel className="w-full mx-auto">
+                <CarouselContent>
+                    {images.map((item, index) => (
+                        <CarouselItem key={index}>
+                            <div className="relative h-[230px]">
+                                <Image 
+                                    alt="prodct image" 
+                                    src={item} 
+                                    fill 
+                                    className="object-cover w-full h-full rounded-lg"
+                                />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                    <CarouselPrevious className="ml-16" />
+                    <CarouselNext className="mr-16" />
+            </Carousel>
+            
+
+            <div className="flex justify-between items-center mt-2">
+                <h1 className="font-semibold text-xl">{name}</h1>
+                <h3 
+                    className="inline-flex items-center rounded-md bg-primary/10 px-2 py-1 
+                                font-medium text-primary ring-1 ring-inset ring-primary/10"
+                >
+                        shs. {price}
+                    </h3>
+            </div>
+
+            <p className="text-gray-600 line-clamp-2 text-sm mt-2">{smallDescription}</p>
+
+            <Button asChild className="w-full mt-5">
+                <Link href={`/products/${id}`}>
+                    Learn More!                
+                </Link>
+
+            </Button>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import { NewestProducts } from "./components/NewestProducts";
 
 
 
 export default function Home() {
   return (
-    <div className="">
-      
-      test
-    </div>
+    <section className="max-w-7xl mx-auto px-4 md:px-8 mb-24">
+      <div className="max-w-3xl mx-auto text-2xl sm:text-5xl lg:text-6xl font-semibold text-center">
+        <h1>Find the best Tailwind</h1>
+        <h1 className="text-primary">Templates & Icons</h1>
+        <p className="lg:text-lg text-muted-foreground mx-auto mt-5 w-[90%] font-normal text-base">
+            FrankUi stands out as the premier marketplace for all things related
+            to tailwindcss, offering an unparalleled platform for both sellers 
+            and buyers alike. 
+          </p>
+      </div>
+
+      <NewestProducts />
+    </section>
   );
 }
